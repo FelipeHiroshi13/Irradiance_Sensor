285100ba5726b7e47c0d285c1fd35a1ffd76d909
Felipe_Hiroshi <felipehiroshi13@gmail.com>
2021-05-24 16:05:26 -0400

    Initial Commit

---
+++ IRRADIANCE.cpp
@@ -0,0 +1,6 @@
+#include "IRRADIANCE.h"
+
+
+void IRRADIANCE::setup{
+
+}
---
+++ IRRADIANCE.h
@@ -0,0 +1,12 @@
+#ifndef __IRRADIANCE_H__
+#define __IRRADIANCE__
+
+#include "Arduino.h"
+
+
+class IRRADIANCE{
+
+}
+
+
+#endif
